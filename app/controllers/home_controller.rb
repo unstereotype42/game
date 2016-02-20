5352f1f6900650c753bc90409ccf5d4a5d7a8f30
Itou-T42 <Itou-T42@mail.dnp.co.jp>
2016-02-20 15:42:28 +0900

    commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def top
+    player_id = 1
+    @player = UserMaster.find_by("user_id = ?" , player_id)
+    @jobflag = NaiseiJob.find_by("user_id = ?" , player_id)
+    p @jobflag.end_flag
   end
 end
