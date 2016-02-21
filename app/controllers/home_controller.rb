c2b691d8b947a26ca5752bd728c4abc6c6025bdb
Itou-T42 <Itou-T42@mail.dnp.co.jp>
2016-02-21 22:44:41 +0900

    first mission complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,12 +16,16 @@ class HomeController < ApplicationController
       progress_time = now_time - start_time
 
       if levelup_time > progress_time
+        @complete_flag = 1
         @run_naisei_remain_time = levelup_time - progress_time
+      else
+        @complete_flag = 0
       end
     end
   end
 
   def kaikon
+
     player_id = params[:player_id]
     player = UserMaster.find_by("user_id = ?" , player_id)
     kaikon_lv_from = player.kaikon_lv
@@ -54,8 +58,61 @@ class HomeController < ApplicationController
   end
 
   def chisui
+
     player_id = params[:player_id]
-    p player_id
+    player = UserMaster.find_by("user_id = ?" , player_id)
+    chisui_lv_from = player.chisui_lv
+    chisui_lv_to = chisui_lv_from + 1
+
+    chisui_info = NaiseiChisuiMaster.find_by("level = ?" , chisui_lv_to)
+    chisui_time = chisui_info.time
+    chisui_cost = chisui_info.cost
+
+    #chisuiに必要なcostをmoneyから減額
+    #（未実装）所持金とコストを比較してマイナスになる場合はエラー画面を表示する
+    remain_money = player.money - chisui_cost
+    player.money = remain_money
+    player.save
+
+    #NaiseiJobテーブルにChisuiレベルアップジョブレコードを挿入
+    player_job = NaiseiJob.find_by("user_id = ?" , player_id)
+
+    chisui_master= NaiseiMaster.find_by("naisei_name = ?" , "chisui")
+    chisui_id = chisui_master.naisei_id
+
+    player_job.naisei_id = chisui_id
+    player_job.level_up = chisui_lv_to
+    player_job.time = chisui_time
+    player_job.start_time = DateTime.now
+    player_job.end_flag = 0
+
+    player_job.save
+
+  end
+
+  def levelup
+
+    player_id = params[:player_id]
+    naisei_id = params[:naisei_id].to_i
+    level_up = params[:level_up]
+
+    player = UserMaster.find_by("user_id = ?" , player_id)
+
+    if naisei_id == 1
+      player.kaikon_lv = level_up
+    elsif naisei_id == 2
+      player.chisui_lv = level_up
+    end
+
+    player.save
+
+    player_job = NaiseiJob.find_by("user_id = ?" , player_id)
+    player_job.end_flag = 1
+    player_job.save
+
+    naisei_name = NaiseiMaster.find_by("naisei_id = ?" , naisei_id)
+    @complete_naisei_name = naisei_name.naisei_name_j
+    @complete_level = level_up
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/top'
   get 'home/kaikon' => 'home#kaikon'
   get 'home/chisui' => 'home#chisui'
+  get 'home/levelup' => 'home#levelup'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
